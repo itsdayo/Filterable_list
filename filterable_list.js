877efc58cdcbef23ca40a9e4830d031c03eee27e
oladayo4351 <oladayoogundipe@Oladayos-Air.fios-router.home>
2018-10-15 14:18:19 -0400

    completed filterlist

--- filterable_list.js
+++ filterable_list.js
@@ -1,18 +1,26 @@
-alert("Hello")
 let searchBar = document.querySelector("#searchBar");
 
-searchBar.addEventListener('keydown', searching)
+searchBar.addEventListener('keyup', searching)
 
 function searching(){
 
 let value = searchBar.value
 
-let names = document.querySelectorAll("li.contact-name")
+let names = document.querySelectorAll("li")
 
-for (let i>0; i < names.length; i++ ){
-let name = names[i]
-	console.log(name)
+for (let i=0; i < names.length; i++ ){
+let name = names[i].innerHTML
+
+if(name.toUpperCase().includes(value.toUpperCase())){
+	names[i].style.display = 'block'
+
 }
+else{
+	names[i].style.display = 'none'
+
+}
+	
+	}
 
 }
 
